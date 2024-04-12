d5ea401f3925300645382bb37b3f4dc1f7e21e00
Sohid Ullah <mdsohidullahchowdhury1@gmail.com>
2024-04-12 22:20:38 +0600

    95% Done

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:weather_app/weather.dart';
-
-
-
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
      builder: (context, child) => MaterialApp(
         debugShowCheckedModeBanner: false,  
         home:WeatherScreen() ,
+        theme: ThemeData(
+          colorScheme: ColorScheme.dark()
+        ),
       ),
       designSize: Size(360, 640),
     );
--- lib/weather.dart
+++ lib/weather.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
 
@@ -8,10 +10,121 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text('Weather App'),
+          centerTitle: true,
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.refresh)),
+          ],
+        ),
 
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 230,
+                width: double.infinity,
+                
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(158, 228, 228, 214),
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                  backgroundBlendMode: BlendMode.hardLight,
+                ),
+              )
+
+              ,SizedBox(height: 15,),
+
+                  Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('Weather Forecast '),
+                    ],
+                  ),
+                  SizedBox(height: 8,),
+                 
+                 SingleChildScrollView(scrollDirection: Axis.horizontal,
+                   child: Row(
+                      children: [
+                        Container( //1
+                          height: 95,
+                          width: 75,
+                          decoration: BoxDecoration(
+                    color: Color.fromARGB(158, 228, 228, 214),
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    backgroundBlendMode: BlendMode.hardLight,
+                                   ),
+                        ),SizedBox(width: 4,),
+                   
+                         Container( //2
+                          height: 95,
+                          width: 75,
+                          decoration: BoxDecoration(
+                    color: Color.fromARGB(158, 228, 228, 214),
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    backgroundBlendMode: BlendMode.hardLight,
+                                   ),
+                        ),SizedBox(width: 4,),
+                   
+                         Container( //3
+                          height: 95,
+                          width: 75,
+                          decoration: BoxDecoration(
+                    color: Color.fromARGB(158, 228, 228, 214),
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    backgroundBlendMode: BlendMode.hardLight,
+                                   ),
+                        ),SizedBox(width: 4,),
+
+                        Container( //4
+                          height: 95,
+                          width: 75,
+                          decoration: BoxDecoration(
+                    color: Color.fromARGB(158, 228, 228, 214),
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    backgroundBlendMode: BlendMode.hardLight,
+                                   ),
+                        ),SizedBox(width: 4,),
+
+                        Container( //5
+                          height: 95,
+                          width: 75,
+                          decoration: BoxDecoration(
+                    color: Color.fromARGB(158, 228, 228, 214),
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    backgroundBlendMode: BlendMode.hardLight,
+                                   ),
+                        ),SizedBox(width: 4,),
+
+                        Container( //6
+                          height: 95,
+                          width: 75,
+                          decoration: BoxDecoration(
+                    color: Color.fromARGB(158, 228, 228, 214),
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    backgroundBlendMode: BlendMode.hardLight,
+                                   ),
+                        ),SizedBox(width: 4,),
+                      ],
+                    ),
+                 ),
+
+                 SizedBox(height: 15,),
+
+                  Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('Additional Information '),
+                    ],
+                  ),
+                  SizedBox(height: 8,),
+
+                  
+            ],
+          ),
+        ),  
     );
   }
 }
